d77720e0002d60ccf395fb34b1b82544dd979fe6
Eagle19243 <eagle19243@gmail.com>
2019-09-11 09:00:06 -0700

    initial completion of navbar

--- src/components/Navbars/Navbar.jsx
+++ src/components/Navbars/Navbar.jsx
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 // reactstrap components
 import {
   Collapse,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
   NavbarBrand,
   Navbar,
   NavItem,
@@ -18,7 +22,6 @@ class PagesNavbar extends React.Component {
     super(props);
     this.state = {
       collapseOpen: false,
-      color: "navbar-transparent"
     };
   }
   componentDidMount() {
@@ -63,7 +66,7 @@ class PagesNavbar extends React.Component {
   render() {
     return (
       <Navbar
-        className={"fixed-top " + this.state.color}
+        className={"fixed-top"}
         color-on-scroll="100"
         expand="lg"
       >
@@ -76,8 +79,8 @@ class PagesNavbar extends React.Component {
               title="Designed and Coded by Creative Tim"
               tag={Link}
             >
-              <span>BLK• </span>
-              Design System React
+              <span>Horseco </span>
+              the horse company
             </NavbarBrand>
             <button
               aria-expanded={this.state.collapseOpen}
@@ -115,50 +118,49 @@ class PagesNavbar extends React.Component {
               </Row>
             </div>
             <Nav navbar>
-              <NavItem className="p-0">
-                <NavLink
-                  data-placement="bottom"
-                  href="https://twitter.com/CreativeTim"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  title="Follow us on Twitter"
-                >
-                  <i className="fab fa-twitter" />
-                  <p className="d-lg-none d-xl-none">Twitter</p>
+              <UncontrolledDropdown nav>
+                <DropdownToggle caret id="navbar_dropdown_auctions" color="default" data-toggle="dropdown" nav>
+                    <p>Auctions</p>
+                </DropdownToggle>
+                <DropdownMenu aria-labelledby="navbar_dropdown_auctions">
+                  <DropdownItem onClick={e => e.preventDefault}>
+                    Item1
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <UncontrolledDropdown nav>
+                <DropdownToggle caret id="navbar_dropdown_catalogs" color="default" data-toggle="dropdown" nav>
+                    <p>Catalogs</p>
+                </DropdownToggle>
+                <DropdownMenu aria-labelledby="navbar_dropdown_catalogs">
+                  <DropdownItem onClick={e => e.preventDefault}>
+                    Item1
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <NavLink onClick={e => e.preventDefault()}>
+                  <p>Enter a Horse</p>
                 </NavLink>
               </NavItem>
-              <NavItem className="p-0">
-                <NavLink
-                  data-placement="bottom"
-                  href="https://www.facebook.com/CreativeTim"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  title="Like us on Facebook"
-                >
-                  <i className="fab fa-facebook-square" />
-                  <p className="d-lg-none d-xl-none">Facebook</p>
-                </NavLink>
-              </NavItem>
-              <NavItem className="p-0">
-                <NavLink
-                  data-placement="bottom"
-                  href="https://www.instagram.com/CreativeTimOfficial"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  title="Follow us on Instagram"
-                >
-                  <i className="fab fa-instagram" />
-                  <p className="d-lg-none d-xl-none">Instagram</p>
+              <UncontrolledDropdown nav>
+                <DropdownToggle caret id="navbar_dropdown_about" color="default" data-toggle="dropdown" nav>
+                    <p>About</p>
+                </DropdownToggle>
+                <DropdownMenu aria-labelledby="navbar_dropdown_about">
+                  <DropdownItem onClick={e => e.preventDefault}>
+                    Item1
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <NavLink onClick={e => e.preventDefault()}>
+                  <p>Register/Login</p>
                 </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} to="/">
-                  Back to Kit
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/creativetimofficial/blk-design-system-react/issues">
-                  Have an issue?
+                <NavLink onClick={e => e.preventDefault()}>
+                  <i aria-hidden={true} className="tim-icons icon-zoom-split"/>
                 </NavLink>
               </NavItem>
             </Nav>
