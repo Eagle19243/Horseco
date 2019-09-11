7ba38d1db79e78f53f6f9103e937687cba830bd3
Eagle19243 <eagle19243@gmail.com>
2019-09-11 11:54:07 -0700

    complete header page

--- src/views/pages/Home.jsx
+++ src/views/pages/Home.jsx
@@ -35,97 +35,17 @@ class Home extends React.Component {
         <Navbar />
         <div className="wrapper">
           <div className="page-header">
-            <img
-              alt="..."
-              className="path"
-              src={require("assets/img/blob.png")}
-            />
-            <img
-              alt="..."
-              className="path2"
-              src={require("assets/img/path2.png")}
-            />
-            <img
-              alt="..."
-              className="shapes triangle"
-              src={require("assets/img/triunghiuri.png")}
-            />
-            <img
-              alt="..."
-              className="shapes wave"
-              src={require("assets/img/waves.png")}
-            />
-            <img
-              alt="..."
-              className="shapes squares"
-              src={require("assets/img/patrat.png")}
-            />
-            <img
-              alt="..."
-              className="shapes circle"
-              src={require("assets/img/cercuri.png")}
-            />
             <div className="content-center">
+              <img alt="..." src={require("assets/img/HORSECO_LOGO.png")} />
+              <p className="mb-4" style={{color: '#115740', fontSize: '18px', fontWeight: '500'}}>
+                Founded by a unique blend of horsemen, owners, technologists, and entrepreneurs.
+              </p>
               <Row className="row-grid justify-content-between align-items-center text-left">
-                <Col lg="6" md="6">
-                  <h1 className="text-white">
-                    We keep your coin <br />
-                    <span className="text-white">secured</span>
-                  </h1>
-                  <p className="text-white mb-3">
-                    A wonderful serenity has taken possession of my entire soul,
-                    like these sweet mornings of spring which I enjoy with my
-                    whole heart. I am alone, and feel...
-                  </p>
-                  <div className="btn-wrapper mb-3">
-                    <p className="category text-success d-inline">
-                      From 9.99%/mo
-                    </p>
-                    <Button
-                      className="btn-link"
-                      color="success"
-                      href="#pablo"
-                      onClick={e => e.preventDefault()}
-                      size="sm"
-                    >
-                      <i className="tim-icons icon-minimal-right" />
-                    </Button>
-                  </div>
-                  <div className="btn-wrapper">
-                    <div className="button-container">
-                      <Button
-                        className="btn-icon btn-simple btn-round btn-neutral"
-                        color="default"
-                        href="#pablo"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <i className="fab fa-twitter" />
-                      </Button>
-                      <Button
-                        className="btn-icon btn-simple btn-round btn-neutral"
-                        color="default"
-                        href="#pablo"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <i className="fab fa-dribbble" />
-                      </Button>
-                      <Button
-                        className="btn-icon btn-simple btn-round btn-neutral"
-                        color="default"
-                        href="#pablo"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <i className="fab fa-facebook" />
-                      </Button>
-                    </div>
-                  </div>
+                <Col lg="6" md="6" sm="6" xs="12">
+                  <Button className="p-0"><img alt="..." src={require("assets/img/Enter_A_Horse.png")} /></Button>
                 </Col>
-                <Col lg="4" md="5">
-                  <img
-                    alt="..."
-                    className="img-fluid"
-                    src={require("assets/img/etherum.png")}
-                  />
+                <Col lg="6" md="6" sm="6" xs="12">
+                  <Button className="p-0"><img alt="..." src={require("assets/img/Register_Login.png")} /></Button>
                 </Col>
               </Row>
             </div>
